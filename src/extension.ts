0df4cfe9fc0898843a37a8985f9cb3ad67aa29d3
Ivan Towlson <itowlson@microsoft.com>
2019-04-12 15:26:44 +1200

    Naive merge Azure kubeconfig into existing

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 import * as vscode from 'vscode';
 // import { ContainerServiceClient } from '@azure/arm-containerservice';
 import * as azcs from 'azure-arm-containerservice';  // deprecated, but @azure/arm-containerservice doesn't play nicely with AzureAccount, so...
+import * as fs from 'fs';
+import * as path from 'path';
+import * as yaml from 'js-yaml';
 
 import { AKSTreeProvider, AKSClusterTreeNode } from './aks-tree';
 
@@ -25,7 +28,7 @@ async function addToKubeconfig(target: AKSClusterTreeNode): Promise<void> {
     try {
         const accessProfile = await client.managedClusters.getAccessProfile(resourceGroupName, name, 'clusterUser');
         const kubeconfig = accessProfile.kubeConfig!.toString();  // TODO: safely
-        console.log(kubeconfig);
+        mergeToKubeconfig(kubeconfig);
     } catch (e) {
         console.log(e);
     }
@@ -41,4 +44,57 @@ function parseResource(armId: string): { resourceGroupName: string | undefined, 
 function bitAfter(bits: string[], after: string): string | undefined {
     const afterIndex = bits.indexOf(after);
     return bits[afterIndex + 1];
+}
+
+interface Named {
+    readonly name: string;
+}
+
+async function mergeToKubeconfig(newConfig: string): Promise<void> {
+    // strategy: find existing kubeconfig (tricky because it could be merged and we really want to pick one file)
+    // parse its YAML
+    // merge sections: clusters, contexts, users (indexing on name property)
+    // save as YAML
+
+    const kcfile = path.join((process.env['HOME'] || process.env['USERPROFILE'] || '.'), ".kube", "config");
+    if (!fs.existsSync(kcfile)) {
+        vscode.window.showErrorMessage("Couldn't find kubeconfig file to merge into");
+        return;
+    }
+
+    const kc = yaml.safeLoad(fs.readFileSync(kcfile, 'utf8'));
+    const nc = yaml.safeLoad(newConfig);
+
+    for (const section of ['clusters', 'contexts', 'users']) {
+        const existing: Named[] | undefined = kc[section];
+        const toMerge: Named[] | undefined = nc[section];
+        if (!existing) {
+            kc[section] = nc[section];
+            continue;
+        }
+        if (!toMerge) {
+            continue;
+        }
+        mergeInfo(existing, toMerge);
+    }
+
+    const merged = yaml.safeDump(kc, { lineWidth: 1000000, noArrayIndent: true });
+    const backup = kcfile + '.before-aks-tools';
+    if (fs.existsSync(backup)) {
+        fs.unlinkSync(backup);
+    }
+    fs.renameSync(kcfile, backup);
+    fs.writeFileSync(kcfile, merged);
+
+    // TODO: should we have an option to 'save in new kubeconfig'?
+}
+
+function mergeInfo(existing: Named[], toMerge: Named[]): void {
+    for (const toMergeEntry of toMerge) {
+        if (existing.some((e) => e.name === toMergeEntry.name)) {
+            // we have CONFLICT and CONFLICT BUILDS CHARACTER
+            continue;  // TODO: build character
+        }
+        existing.push(toMergeEntry);
+    }
 }
