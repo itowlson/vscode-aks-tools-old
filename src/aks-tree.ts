4689860adc22a625b0a573dab2e297baf57b8935
Ivan Towlson <itowlson@microsoft.com>
2019-04-17 10:45:59 +1200

    Cloud root nodes are no longer passed to providers

--- src/aks-tree.ts
+++ src/aks-tree.ts
@@ -19,8 +19,8 @@ export class AKSTreeProvider implements vscode.TreeDataProvider<AKSTreeNode> {
         }
     }
 
-    getChildren(element?: AKSTreeNode | { nodeType: 'cloud' } | undefined): vscode.ProviderResult<AKSTreeNode[]> {
-        if (!element || element.nodeType === 'cloud') {
+    getChildren(element?: AKSTreeNode | undefined): vscode.ProviderResult<AKSTreeNode[]> {
+        if (!element) {
             return subscriptions();
         } else if (element.nodeType === 'subscription') {
             return clusters(element.session, element.subscription);
